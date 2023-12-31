04093dfe4775af039a0edc4b648ed4e7df4bf836
j33h22 <ju33huang22@gmail.com>
2023-12-31 11:11:03 +0800

    Separate the homepage codes to a new vue file
    The new vue file is in "src/components/Articles"

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
-import VersionList from './components/Articles/VersionList.vue'
-import CompareVersion from './components/Articles/CompareVersion.vue'
+import HomepageBase from './components/Articles/HomepageBase.vue'
 import 'mdui/mdui.css';
 import 'mdui';
 import 'mdui/components/top-app-bar.js';
@@ -17,26 +16,5 @@ setColorScheme('#0061a4');
 </script>
 
 <template>
-  <mdui-layout>
-  <mdui-top-app-bar>
-    <mdui-top-app-bar-title>原神在线TTS</mdui-top-app-bar-title>
-  </mdui-top-app-bar>
-  <mdui-layout-main style="min-height: 300px;">
-
-  <div class="mdui-prose container">
-    <h1>目前可用的版本</h1>
-    <VersionList />
-    <br />
-    <h1>版本功能对比</h1>
-    <CompareVersion />
-    <p>© 2023 Stardust·减</p>
-  </div>
-</mdui-layout-main>
-</mdui-layout>
+  <HomepageBase />
 </template>
-
-<style scoped>
-.container {
-  padding: 1rem 1rem;
-}
-</style>
---
+++ src/components/Articles/HomepageBase.vue
@@ -0,0 +1,29 @@
+<script setup>
+import VersionList from './VersionList.vue'
+import CompareVersion from './CompareVersion.vue'
+</script>
+
+<template>
+    <mdui-layout>
+        <mdui-top-app-bar>
+            <mdui-top-app-bar-title>原神在线TTS</mdui-top-app-bar-title>
+        </mdui-top-app-bar>
+        <mdui-layout-main style="min-height: 300px;">
+
+            <div class="mdui-prose container">
+                <h1>目前可用的版本</h1>
+                <VersionList />
+                <br />
+                <h1>版本功能对比</h1>
+                <CompareVersion />
+                <p>© 2023 Stardust·减</p>
+            </div>
+        </mdui-layout-main>
+    </mdui-layout>
+</template>
+
+<style scoped>
+.container {
+  padding: 1rem 1rem;
+}
+</style>
